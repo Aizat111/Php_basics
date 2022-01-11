bb162f4efb66875df85438ba8f99a86a583a7fe9
Aizat111 <aizaesen4@gmail.com>
2022-01-11 21:38:49 +0300

    run session on another page

---
+++ session2.php
@@ -0,0 +1,7 @@
+<?php
+//sessions sayfasinda tanimladigimiz verileri oturumu kapatmadigimiz surece burada da acabiliriz
+session_start();
+echo 'The value of animal: '.$_SESSION['animal'];
+echo '<br> The color is: '.$_SESSION['color'];
+echo '<br>Time is: '.$_SESSION['_time'];
+?>
