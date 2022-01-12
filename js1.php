6f76c2d50feef1507d5243a8dcd8a677641ae091
Aizat111 <aizaesen4@gmail.com>
2022-01-12 21:50:03 +0300

    onChange,onFocus,onBlur,onKeyPress events in js

---
+++ js1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>JS 1.ornek</title>
+    </head>
+    <body>
+    
+    <script type="text/javascript">
+    function Goster(metin){
+        alert("Metin kurusundaki son deger "+metin+"'dir");
+    }    
+    function Pasif(ttt){
+        document.test1.bilgi.value=ttt.name+" pasif";
+    }
+    function Aktif(object){
+        document.test1.bilgi.value=object.name + " aktif";
+    }
+    </script>
+    
+    <form name="test1">
+    Adi <input type="text" name="adi" value="" onChange="Goster(this.value)" onFocus="Aktif(this)" onBlur="Pasif(this)" >
+
+    Soyadi <input type="text" name="soyadi" value="" onKeyPress="Goster(document.test1s.soyadi.value">
+    Bilgi <input type="text" name="bilgi" value="">
+    </form>
+    </body>
+</html>
