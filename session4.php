fe55fbc241ac2d279ddaafdc417e7ec4fe61044b
Aizat111 <aizaesen4@gmail.com>
2022-01-19 10:35:15 +0300

    append new value to session

---
+++ session4.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    session_start();
+    ?>
+    <div class="login">
+        <center>
+    <form action="session4.php"  method="post" >
+        <input type="text" name="name"> <br><br>
+        <input type="password" name="psd"> <br><br>
+        <input type="Submit" value="Ok"><br><br>
+    </form>
+    </div>
+</center>
+    <div class="text">
+        <?php
+        if(isset($_POST['name'])&&isset($_POST['psd']))
+        {
+         
+            $username=$_POST['name'];
+            $password=$_POST['psd'];
+            if(isset($_SESSION['username']))
+            {
+                $_SESSION['username'].="<br>";
+                $_SESSION['username'].=$username;
+            }
+            else
+            {
+                $_SESSION['username']=$username;
+            }
+            echo $_SESSION['username'];
+            
+        }
+        ?>
+    </div>
+  
+ 
+
+
+</body>
+</html>
